35e928c3a83d62e3fbd7a346266b844bc285d7d0
nayakrujul <nayakrujul@hotmail.com>
2023-07-08 18:02:53 +0100

    Verbose mode!

--- run/main.js
+++ run/main.js
@@ -1,3 +1,310 @@
+let dictionary = new Map();
+
+dictionary.set([65], ["abs", "absolute", "isalpha", "is_alpha"]);
+dictionary.set([66], ["to_base", "from_decimal"]);
+dictionary.set([67], ["chr", "character", "ord", "ordinal"]);
+dictionary.set([68], ["dup", "duplicate"]);
+dictionary.set([69], ["even", "is_even", "eval"]);
+dictionary.set([70], ["factors", "substrings"]);
+dictionary.set([71], ["max", "maximum", "greatest"]);
+dictionary.set([72], ["from_hex", "from_hexadecimal"]);
+dictionary.set([73], ["inclusive_range", "slice", "interleave"]);
+dictionary.set([74], ["empty_join", "str"]);
+dictionary.set([76], ["lowered_range", "zero_range", "lower", "lowercase"]);
+dictionary.set([77], ["min", "minimum"]);
+dictionary.set([78], ["int", "integer"]);
+dictionary.set([79], ["two_power", "space_split"]);
+dictionary.set([80], ["prime", "is_prime", "swapcase"]);
+dictionary.set([81], ["not_equal", "inequality"]);
+dictionary.set([82], ["one_range", "range", "upper", "uppercase"]);
+dictionary.set([83], ["sum"]);
+dictionary.set([84], ["ten"]);
+dictionary.set([85], ["uniquify"]);
+dictionary.set([86], ["where_truthy", "rot13"]);
+dictionary.set([87], ["wrap"]);
+dictionary.set([90], ["zip"]);
+dictionary.set([97], ["append"]);
+dictionary.set([98], ["from_base", "to_decimal"]);
+dictionary.set([99], ["ncr", "count"]);
+dictionary.set([100], ["digits", "characters", "chars"]);
+dictionary.set([101], ["ten_power", "comma_split"]);
+dictionary.set([102], ["prime_factors", "case"]);
+dictionary.set([103], ["gcd", "greatest_common_divisor"]);
+dictionary.set([104], ["head", "first"]);
+dictionary.set([105], ["indexing", "zero_indexing"]);
+dictionary.set([106], ["join"]);
+dictionary.set([108], ["len", "length"]);
+dictionary.set([109], ["mean", "avg", "average"]);
+dictionary.set([111], ["rmv", "remove"]);
+dictionary.set([112], ["product", "prod"]);
+dictionary.set([114], ["reverse"]);
+dictionary.set([115], ["swap"]);
+dictionary.set([116], ["tail", "last"]);
+dictionary.set([117], ["negative_one"]);
+dictionary.set([118], ["replace"]);
+dictionary.set([119], ["factorial", "remove_whitespace"]);
+dictionary.set([122], ["uninterleave"]);
+dictionary.set([550], ["any", "is_alphanum", "is_alpha_num"]);
+dictionary.set([7682], ["from_binary"]);
+dictionary.set([266], ["codepage_chr", "codepage_character", "codepage_ord", "codepage_ordinal"]);
+dictionary.set([7690], ["divisible", "is_divisible"]);
+dictionary.set([278], ["enumerate", "inclusive_zero_range"]);
+dictionary.set([7710], ["flatten", "recursive_flatten"]);
+dictionary.set([288], ["dyadic_gcd"]);
+dictionary.set([7714], ["head_extract"]);
+dictionary.set([304], ["one_indexing"]);
+dictionary.set([319], ["ljust", "left_justify"]);
+dictionary.set([7744], ["mode", "most_common_element"]);
+dictionary.set([7748], ["negate", "neg", "rle", "run_length_encode"]);
+dictionary.set([558], ["index_of", "find"]);
+dictionary.set([7766], ["cartesian_product"]);
+dictionary.set([7768], ["repr"]);
+dictionary.set([7776], ["sort"]);
+dictionary.set([7786], ["tail_extract"]);
+dictionary.set([7814], ["chunk", "chunk_split"]);
+dictionary.set([379], ["zero_length_range"]);
+dictionary.set([44], ["pair"]);
+dictionary.set([8314], ["increment", "incr", "plus_one"]);
+dictionary.set([8315], ["decrement", "decr", "minus_one"]);
+dictionary.set([8316], ["exactly_equal", "exactly_equals"]);
+dictionary.set([43], ["add", "plus"]);
+dictionary.set([45], ["subtract", "minus"]);
+dictionary.set([215], ["multiply", "times"]);
+dictionary.set([47], ["divide", "split"]);
+dictionary.set([42], ["exponentiate", "power", "pow", "regex", "findall"]);
+dictionary.set([37], ["modulo", "mod", "format"]);
+dictionary.set([247], ["floor_divide", "integer_divide"]);
+dictionary.set([95], ["swapped_subtract", "swapped_minus"]);
+dictionary.set([64], ["swapped_exponentiate", "swapped_pow", "swapped_findall", "swapped_regex"]);
+dictionary.set([338], ["swapped_modulo", "swapped_mod", "swapped_format"]);
+dictionary.set([166], ["swapped_floor_divide", "swapped_integer_divide"]);
+dictionary.set([61], ["equal", "equals"]);
+dictionary.set([60], ["less_than"]);
+dictionary.set([62], ["greater_than"]);
+dictionary.set([169], ["less_than_or_equal_to"]);
+dictionary.set([174], ["greater_than_or_equal_to"]);
+dictionary.set([38], ["and", "logical_and"]);
+dictionary.set([124], ["or", "logical_or"]);
+dictionary.set([94], ["uninterleave_dump"]);
+dictionary.set([126], ["not", "logical_not"]);
+dictionary.set([240], ["space"]);
+dictionary.set([208], ["triplicate"]);
+dictionary.set([551], ["assign"]);
+dictionary.set([7683], ["bin", "binary", "to_binary"]);
+dictionary.set([267], ["combinations", "union"]);
+dictionary.set([7691], ["divmod", "from_digits"]);
+dictionary.set([279], ["length_range", "exclusive_one_range"]);
+dictionary.set([7711], ["prime_factor_exponents", "title", "title_case"]);
+dictionary.set([289], ["group_consecutive"]);
+dictionary.set([7715], ["head_remove"]);
+dictionary.set([320], ["lcm", "lowest_common_multiple"]);
+dictionary.set([7745], ["median"]);
+dictionary.set([559], ["set_intersection", "round_dps"]);
+dictionary.set([7767], ["permutations", "set_difference"]);
+dictionary.set([7769], ["rjust", "right_justify"]);
+dictionary.set([7777], ["cumsum", "cumulative_sums"]);
+dictionary.set([7787], ["tail_remove"]);
+dictionary.set([7815], ["chunk_wrap", "split_chunk"]);
+dictionary.set([380], ["length_range_no_pop"]);
+dictionary.set([7840], ["alphabet", "lowercase_alphabet"]);
+dictionary.set([7684], ["to_base_string", "character_multiply", "regex_split"]);
+dictionary.set([7692], ["double"]);
+dictionary.set([7864], ["dump"]);
+dictionary.set([7716], ["hex", "hexadecimal", "to_hexadecimal"]);
+dictionary.set([7882], ["reciprocal", "inverse", "filter_isalpha", "only_alphabetic"]);
+dictionary.set([7730], ["bifurcate"]);
+dictionary.set([7734], ["dyadic_lcm"]);
+dictionary.set([7746], ["mirror"]);
+dictionary.set([7750], ["transliterate"]);
+dictionary.set([7884], ["dyadic_minimum"]);
+dictionary.set([7770], ["combinations_with_replacement"]);
+dictionary.set([7778], ["deltas", "forward_differences"]);
+dictionary.set([7788], ["transpose"]);
+dictionary.set([7908], ["rotate_left_once"]);
+dictionary.set([7806], ["rotate_right_once"]);
+dictionary.set([7816], ["uninterleave_into_pieces"]);
+dictionary.set([7924], ["rotate_left", "left_bit_shift"]);
+dictionary.set([7826], ["rotate_right", "right_bit_shift"]);
+dictionary.set([196], ["num_to_alphabet", "alphabet_to_num"]);
+dictionary.set([182], ["newline"]);
+dictionary.set([199], ["pipe"]);
+dictionary.set([172], ["non_vectorised_not", "non_vectorised_logical_not"]);
+dictionary.set([167], ["dyadic_maximum"]);
+dictionary.set([189], ["halve"]);
+dictionary.set([391], ["npr", "contains"]);
+dictionary.set([394], ["drop_at_index", "remove_at_index"]);
+dictionary.set([401], ["unique_prime_factors", "sublists"]);
+dictionary.set([403], ["sixteen"]);
+dictionary.set([11374], ["palindromise"]);
+dictionary.set([413], ["integer_partitions", "newline_join", "join_by_newlines"]);
+dictionary.set([420], ["prepend"]);
+dictionary.set([428], ["transpose_with_filler"]);
+dictionary.set([595], ["boolify"]);
+dictionary.set([392], ["counts"]);
+dictionary.set([599], ["parity", "second_half"]);
+dictionary.set([402], ["prefixes"]);
+dictionary.set([608], ["two_fifty_six", "two_hundred_fifty_six"]);
+dictionary.set([614], ["hundred", "one_hundred"]);
+dictionary.set([409], ["grade_up", "increment_twice"]);
+dictionary.set([625], ["head_slice"]);
+dictionary.set([626], ["tail_slice"]);
+dictionary.set([421], ["pop"]);
+dictionary.set([672], ["powerset"]);
+dictionary.set([636], ["random", "random_choice", "randint"]);
+dictionary.set([642], ["sign", "sentence_case", "sum_each"]);
+dictionary.set([429], ["square_root", "every_second_item"]);
+dictionary.set([178], ["square"]);
+dictionary.set([179], ["cube"]);
+dictionary.set([8308], ["fourth", "fourth_power"]);
+dictionary.set([8309], ["fifth", "fifth_power"]);
+dictionary.set([7841], ["all_equal"]);
+dictionary.set([7685], ["equals_one"]);
+dictionary.set([7693], ["symmetric_set_difference"]);
+dictionary.set([7865], ["enclose"]);
+dictionary.set([7717], ["concatenate", "merge"]);
+dictionary.set([7883], ["corresponding_filter"]);
+dictionary.set([7731], ["sorted_uniquify"]);
+dictionary.set([7735], ["length_of_each", "complement", "is_lower"]);
+dictionary.set([7747], ["ceil", "is_vowel", "reverse_each"]);
+dictionary.set([7885], ["repeat"]);
+dictionary.set([7771], ["absolute_difference", "zfill", "surround"]);
+dictionary.set([7779], ["suffixes", "spaces"]);
+dictionary.set([7789], ["triple_swap"]);
+dictionary.set([7817], ["cartesian_power"]);
+dictionary.set([7925], ["head_remove_many"]);
+dictionary.set([7827], ["tail_remove_many"]);
+dictionary.set([248, 66], ["balanced_brackets", "brackets_are_balanced"]);
+dictionary.set([248, 68], ["optimal_dictionary_compression"]);
+dictionary.set([248, 118], ["canvas", "draw", "canvas_draw"]);
+dictionary.set([248, 86], ["blank_canvas", "blank_draw", "blank_canvas_draw"]);
+dictionary.set([248, 94], ["clear_canvas"]);
+dictionary.set([248, 60], ["starts_with"]);
+dictionary.set([248, 62], ["starts_with"]);
+dictionary.set([216, 67], ["center", "centre"]);
+dictionary.set([216, 68], ["depth"]);
+dictionary.set([216, 69], ["longest"]);
+dictionary.set([216, 73], ["multidimensional_index"]);
+dictionary.set([216, 77], ["shortest"]);
+dictionary.set([216, 46], ["dot_product"]);
+dictionary.set([216, 47], ["anti_diagonal"]);
+dictionary.set([216, 8220], ["all_diagonals"]);
+dictionary.set([216, 8221], ["all_anti_diagonals"]);
+dictionary.set([198, 67], ["cosine", "cos"]);
+dictionary.set([198, 68], ["degrees"]);
+dictionary.set([198, 69], ["exponent"]);
+dictionary.set([198, 70], ["nth_fibonacci_number"]);
+dictionary.set([198, 72], ["hypot", "hypotenuse"]);
+dictionary.set([198, 73], ["insignificant"]);
+dictionary.set([198, 76], ["log", "logarithm"]);
+dictionary.set([198, 78], ["natural_log", "natural_logarithm"]);
+dictionary.set([198, 80], ["nth_prime"]);
+dictionary.set([198, 82], ["radians"]);
+dictionary.set([198, 83], ["sine", "sin"]);
+dictionary.set([198, 84], ["tangent", "tan"]);
+dictionary.set([198, 99], ["arc_cosine", "arccos"]);
+dictionary.set([198, 108], ["log10", "common_log", "common_logarithm"]);
+dictionary.set([198, 115], ["arc_sine", "arcsin"]);
+dictionary.set([198, 116], ["arc_tangent", "arctan"]);
+dictionary.set([198, 7735], ["log2", "log_base_2"]);
+dictionary.set([198, 38], ["bitwise_and"]);
+dictionary.set([198, 124], ["bitwise_or"]);
+dictionary.set([198, 94], ["bitwise_xor"]);
+dictionary.set([198, 126], ["bitwise_not"]);
+dictionary.set([198, 178], ["perfect_square"]);
+dictionary.set([198, 179], ["perfect_cube"]);
+dictionary.set([198, 8308], ["perfect_fourth"]);
+dictionary.set([198, 8309], ["perfect_fifth"]);
+dictionary.set([181, 82], ["roman_numerals"]);
+dictionary.set([181, 84], ["type"]);
+dictionary.set([181, 85], ["connected_uniquify"]);
+dictionary.set([181, 114], ["random_shuffle"]);
+dictionary.set([181, 118], ["trim"]);
+dictionary.set([181, 60], ["left_trim"]);
+dictionary.set([181, 62], ["right_trim"]);
+dictionary.set([181, 38], ["non_vectorised_and", "non_vectorised_logical_and"]);
+dictionary.set([181, 124], ["non_vectorised_or", "non_vectorised_logical_or"]);
+dictionary.set([181, 94], ["non_vectorised_xor", "non_vectorised_logical_xor"]);
+dictionary.set([181, 47], ["split_on"]);
+dictionary.set([34], ["string_literal"]);
+dictionary.set([39], ["one_character"]);
+dictionary.set([96], ["two_characters"]);
+dictionary.set([651], ["three_characters"]);
+dictionary.set([8220], ["lowercase_string_compression"]);
+dictionary.set([8221], ["title_case_string_compression"]);
+dictionary.set([8216], ["lowercase_dictionary_compression"]);
+dictionary.set([8217], ["title_case_dictionary_compression"]);
+dictionary.set([187], ["integer_compression"]);
+dictionary.set([171], ["small_integer_compression"]);
+dictionary.set([191], ["integer_list_compression"]);
+dictionary.set([91], ["open_list_literal", "open_list", "list"]);
+dictionary.set([93], ["close_list_literal", "close_list", "end_list"]);
+dictionary.set([35], ["comment"]);
+dictionary.set([32], ["nop"]);
+dictionary.set([161], ["get_veriable"]);
+dictionary.set([33], ["set_variable"]);
+dictionary.set([305], ["map"]);
+dictionary.set([8364], ["single_function_map"]);
+dictionary.set([230], ["filter"]);
+dictionary.set([339], ["single_function_filter"]);
+dictionary.set([222], ["sort_by"]);
+dictionary.set([254], ["single_function_sort_by"]);
+dictionary.set([209], ["group_by"]);
+dictionary.set([241], ["single_function_group_by"]);
+dictionary.set([567], ["outer_product"]);
+dictionary.set([165], ["fixed_point"]);
+dictionary.set([123], ["open_for_loop"]);
+dictionary.set([125], ["close_for_loop"]);
+dictionary.set([40], ["open_while_loop"]);
+dictionary.set([41], ["close_while_loop"]);
+dictionary.set([8317], ["open_forever_loop"]);
+dictionary.set([8318], ["close_forever_loop"]);
+dictionary.set([63], ["if_statement", "if"]);
+dictionary.set([58], ["else_statement", "else"]);
+dictionary.set([59], ["close_statement"]);
+dictionary.set([231], ["pair_apply"]);
+dictionary.set([110], ["context_variable"]);
+dictionary.set([7749], ["iteration_index"]);
+dictionary.set([120], ["get_x"]);
+dictionary.set([121], ["get_y"]);
+dictionary.set([88], ["set_x"]);
+dictionary.set([89], ["set_y"]);
+dictionary.set([7818], ["set_x_without_popping"]);
+dictionary.set([7822], ["set_y_without_popping"]);
+dictionary.set([7819], ["apply_to_x"]);
+dictionary.set([7823], ["apply_to_y"]);
+dictionary.set([548], ["get_global_array"]);
+dictionary.set([549], ["add_to_global_array"]);
+dictionary.set([75], ["stack"]);
+dictionary.set([7751], ["codepage_compression"]);
+dictionary.set([113], ["quit"]);
+dictionary.set([36], ["next_input"]);
+dictionary.set([164], ["input_list"]);
+dictionary.set([176], ["first_input"]);
+dictionary.set([185], ["second_input"]);
+dictionary.set([8310], ["third_input"]);
+dictionary.set([8311], ["third_last_input"]);
+dictionary.set([8312], ["second_last_input"]);
+dictionary.set([8313], ["last_input"]);
+dictionary.set([163], ["print"]);
+dictionary.set([162], ["print_without_newline"]);
+dictionary.set([223], ["print_without_popping"]);
+dictionary.set([408], ["first_n_integers"]);
+dictionary.set([434], ["cumulative_reduce_by"]);
+dictionary.set([385], ["execute_without_popping"]);
+dictionary.set([181, 181], ["recursive_environment"]);
+dictionary.set([181, 434], ["single_function_reduce_by"]);
+dictionary.set([181, 636], ["single_function_right_reduce_by"]);
+dictionary.set([181, 391], ["single_function_right_cumulative_reduce_by"]);
+dictionary.set([181, 651], ["right_reduce_by"]);
+dictionary.set([181, 8364], ["apply_to_every_nth_item"]);
+dictionary.set([181, 171], ["rotate_stack_left"]);
+dictionary.set([181, 187], ["rotate_stack_right"]);
+dictionary.set([181, 33], ["reverse_stack"]);
+dictionary.set([181, 209], ["adjacent_group_by"]);
+dictionary.set([181, 241], ["single_function_adjacent_group_by"]);
+
+let keys = [...dictionary.keys()];
+
 var running = false;
 
 const bytecount = document.getElementById("bytecount");
@@ -80,7 +387,10 @@ function button_clicked() {
     if (header !== "") {
         header += "\n";
     }
-    const code = document.getElementById("code").value;
+    let code = document.getElementById("code").value;
+    if (document.getElementById("flags").value.includes("v")) {
+        code = document.getElementById("verbose").innerText;
+    }
     let footer = document.getElementById("footer").value;
     if (footer !== "") {
         footer = "\n" + footer;
@@ -131,7 +441,10 @@ async function get_cgcc_post() {
   let url = generate_link();
   const flags = document.getElementById("flags").value;
   flags_md = flags !== "" ? " `" + flags + "`" : "";
-  const code = document.getElementById("code").value;
+  let code = document.getElementById("code").value;
+  if (document.getElementById("flags").value.includes("v")) {
+    code = document.getElementById("verbose").innerText;
+  }
   const plural = document.getElementById("plural").innerText;
   let cgcc_post =
     "# [Thunno 2](https://github.com/Thunno/Thunno2)" + flags_md +
@@ -161,7 +474,10 @@ async function get_cmc() {
   let url = generate_link();
   const flags = document.getElementById("flags").value;
   flags_md = flags !== "" ? " `" + flags + "`" : "";
-  const code = document.getElementById("code").value;
+  let code = document.getElementById("code").value;
+  if (document.getElementById("flags").value.includes("v")) {
+    code = document.getElementById("verbose").innerText;
+  }
   const plural = document.getElementById("plural").innerText;
   let cmc_text = "Thunno 2" + flags_md + ", " + bytecount.innerText + 
     " byte" + plural + ": [" + code + "](" + url + ")";
@@ -192,33 +508,112 @@ function getBytecount() {
   }
 }
 
-function adjustTextareaHeight1() {
+function adjustVerboseMode() {
+  let s = document.getElementById("code").value;
+    
+  let out1 = [];
+  let out2 = "";
+  let arr = s.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i];
+
+    if (word === "" && i < arr.length - 1) {
+      out1.push(" ");
+      continue;
+    }
+
+    if (!isNaN(word)) {
+      out1.push(word);
+      continue;
+    }
+
+    if (word[0] === "\\") {
+      out1.push(word.slice(1));
+      continue;
+    }
+
+    if (!/^[a-zA-Z_]+\d*$/.test(word)) {
+      out1.push(word);
+      continue;
+    }
+
+    let found = false;
+    for (let j = 0; j < keys.length; j++) {
+      let key = keys[j];
+      let val = dictionary.get(key);
+
+      if (val.includes(word.toLowerCase())) {
+        out1.push(String.fromCodePoint(...key));
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      if (i < arr.length - 1) {
+        out1.push(word);
+      } else {
+        out2 = word;
+      }
+    }
+  }
+    
+  let result = out1.join("");
+    
+  document.getElementById("verbose").innerText = result + "\n\n";
+    
+  let utf8 = document.getElementById("utf8");
+  let plural = document.getElementById("plural");
+
+  utf8.innerText = '(Verbose)';
+  bytecount.innerText = '' + result.length;
+    
+  if (+bytecount.innerText === 1) {
+    plural.innerText = '';
+  } else {
+    plural.innerText = 's';
+  }
+}
+
+function adjustTextareaHeight1() { // input
   var textarea = document.getElementById('input');
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
 }
 
-function adjustTextareaHeight2() {
+function adjustTextareaHeight2() { // flags
   var textarea = document.getElementById('flags');
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
+  if (textarea.value.includes("v")) {
+    adjustVerboseMode();
+  } else {
+    document.getElementById("verbose").innerText = "";
+    getBytecount();
+  }
 }
 
-function adjustTextareaHeight3() {
+function adjustTextareaHeight3() { // code
   var textarea = document.getElementById('code');
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
-  getBytecount();
+  if (document.getElementById('flags').value.includes("v")) {
+    adjustVerboseMode();
+  } else {
+    document.getElementById("verbose").innerText = "";
+    getBytecount();
+  }
 }
 
-function adjustTextareaHeight4() {
+function adjustTextareaHeight4() { // header
   var textarea = document.getElementById('header');
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
   getBytecount();
 }
 
-function adjustTextareaHeight5() {
+function adjustTextareaHeight5() { // footer
   var textarea = document.getElementById('footer');
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
